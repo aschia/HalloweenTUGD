2999a57effaf6cb72ae55f8ae0ef9f318baf5359
aschia <ajs1173@utulsa.edu>
2021-10-02 14:51:35 -0500

    Basic movement

--- HalloweenProject/Assets/Scripts/Player.cs
+++ HalloweenProject/Assets/Scripts/Player.cs
@@ -17,9 +17,51 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxColl;
+    private SpriteRenderer spRend;
+
+    private Vector3 moveDelta;
+    public List<Sprite> moveSprites;
+    public int sprIndex;
 
     private void Start()
     {
         boxColl = GetComponent<BoxCollider2D>();
+        spRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //get input
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //reset movedelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //swap sprite direction
+        int newInd = -1;
+        if (moveDelta.x != 0)
+        {
+            newInd = 1;
+            transform.localScale = new Vector3(1 * x, 1, 1);
+        }
+        if (moveDelta.y != 0)
+        {
+            newInd = 0;
+            if (y > 0) newInd = 2;
+        }
+
+        if (newInd != -1)
+        {
+            sprIndex = newInd;
+            Debug.Log(newInd);
+            spRend.sprite = moveSprites[newInd];
+        }
+
+        //move it move it
+        transform.Translate(moveDelta * Time.deltaTime);
+
+        /*Debug.Log(x);
+        Debug.Log(y);*/
     }
 }
